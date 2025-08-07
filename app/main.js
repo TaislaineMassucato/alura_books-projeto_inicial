5ab3f5860f1c504e1fddd6ad1897f370407a8213
Taislaine Massucato <massucato2@outlook.com>
2025-08-07 07:51:14 -0300

    Fetch de livros no console

---
+++ app/main.js
@@ -0,0 +1,12 @@
+let livros = [];
+//requisição link json
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+getBuscarLivrosDaAPI();
+
+async function getBuscarLivrosDaAPI(){
+                      //buscar dados de um servidor
+    const res = await fetch(endpointDaAPI); 
+    // transforma a resposta em JSON
+    livros = await res.json();
+    console.table(livros);
+}
